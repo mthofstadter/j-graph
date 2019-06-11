38ceed7b4a3a9921e7ef9e6c7ffef0498201f121
mthofstadter <31392780+mthofstadter@users.noreply.github.com>
2019-06-11 08:48:17 -0500

    Initial commit

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <link rel='stylesheet' type='text/css' href='style.css' />
+    <script src="main.js"></script>
+  <title>J-Graph</title>
+</head>
+<body>
+
+<div class="header">
+  <h1> J-Graph </h1>
+</div>
+
+<div class="container">
+  <div class="input-body">
+    <label>Fixed Cost:</label>
+    <input placeholder="fixed cost"/>
+
+    <label>Current Ad Spend:</label>
+    <input placeholder="current ad spend"/>
+
+    <label id="change">Revenue:</label>
+    <input placeholder="revenue"/>
+  </div>
+  <div class="graph-body">
+    <!--<iframe src="https://www.desmos.com/calculator/kjgsiieksf?embed"></iframe>-->
+    <iframe src="https://www.desmos.com/calculator/2mesganymt"></iframe>
+  </div>
+</div>
+
+</body>
+</html>
