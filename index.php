7a1d03ce3fa726f264ec1e95a34363c425eaee63
mthofstadter <31392780+mthofstadter@users.noreply.github.com>
2019-06-14 14:48:14 -0500

    New Changes Friday
    Main.js is now in use, started implementing ratio graphing, layout changes so now it doesn't look awful

--- index.php
+++ index.php
@@ -1,111 +1,49 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <link rel='stylesheet' type='text/css' href='style.css' />
-    <script src="main.js"></script>
+  <link rel='stylesheet' type='text/css' href='style.css' />
+  <script src="/j-graph/main.js"></script>
+
   <title>J-Graph</title>
 
   <script>
 
-  var stored_fix = 0;
-  var color = "red";
-
-  function graph() {
-    clear();
-    drawBoard();
-    plotFixed();
-    colorLine();
-    plotAd();
-  }
-
-  function clear() {
-    ctx.beginPath();
-    ctx.clearRect(0,0, c.width, c.height);
-  }
-
-  function plotFixed() {
-    var fixed_cost = document.getElementById("fixed");
-    stored_fix = fixed_cost.value;
-    ctx.beginPath();
-    ctx.moveTo(0, (c.height - fixed_cost.value)); <!--origin-->
-    ctx.lineTo(c.width, (c.height - fixed_cost.value));
-    ctx.strokeStyle = "blue";
-    ctx.stroke();
-  }
-
-  function plotAd() {
-    var ad_spend = document.getElementById("ad");
-    ctx.beginPath();
-    ctx.moveTo(0, (c.height - stored_fix));
-    ctx.lineTo(c.width, (c.height - stored_fix - ad_spend.value));
-    ctx.strokeStyle = color;
-    if(ad_spend.value > 0) {
-      ctx.stroke();
-    }
-  }
-
-function drawBoard() {
-  // Box width
-  var bw = 900;
-  // Box height
-  var bh = 700;
-  // Padding
-  var p = 0;
-
-  ctx.beginPath();
-  ctx.strokeStyle = "gainsboro";
-    for (var x = 0; x <= bw; x += 40) {
-        ctx.moveTo(0.5 + x + p, p);
-        ctx.lineTo(0.5 + x + p, bh + p);
-    }
-
-    for (var x = 0; x <= bh; x += 40) {
-        ctx.moveTo(p, 0.5 + x + p);
-        ctx.lineTo(bw + p, 0.5 + x + p);
-    }
-    ctx.stroke();
-}
-
-function colorLine() {
-  var revBox = document.getElementById("color");
-  color = revBox.value;
-}
-
   </script>
 
 </head>
-<body onload="drawBoard();">
 
-<ul>
-  <li id="main">J-Graph</li>
-  <li><a href="#">About</a></li>
-  <li><a href="#">Reset</a></li>
-</ul>
+<body>
 
+  <ul>
+    <li id="main">J-Graph</li>
+    <li><a href="#" onclick="about()">About</a></li>
+    <li><a href="#" onclick="reset()" id="resetButton">Reset</a></li>
+  </ul>
 
-<div class="graph-body">
-    <canvas id="myCanvas" width="800" height="600"></canvas>
-</div>
+  <div class="main-body">
+  <div class="graph-body">
+      <canvas id="myCanvas" width="1200" height="800"></canvas>
+  </div>
+  <div class="label-body">
+      <label>Fixed Cost:</label>
+      <label>Ad Spend:</label>
+      <label>Revenue:</label>
+  </div>
+  <div class="input-body">
+    <input type="text" id="fixed" onchange="graph()"/>
+    <input id="ad" onchange="graph()"/>
+    <input id="revenue" onchange="graph()"/>
+    <input type="color" value="#ff0000" id="color" onchange="graph()"/>
+  </div>
 
-<div class="input-body">
-  <label>Fixed Cost:</label>
-  <input placeholder="fixed cost" type="text" id="fixed" onchange="graph()"/>
+  </div>
 
-  <label>Current Ad Spend:</label>
-  <input placeholder="current ad spend" id="ad" onchange="graph()"/>
+  <div id="bigolediv"></div>
 
-  <label id="change">Revenue:</label>
-  <input placeholder="revenue" id="revenue" onchange="graph()"/>
+  <script>
+    var c = document.getElementById("myCanvas");
+    var ctx = c.getContext("2d");
+  </script>
 
-  <input type="color" value="#ff0000" id="color" onchange="graph()"/>
-</div>
-
-<div id="bigolediv"></div>
-
-<script>
-  var c = document.getElementById("myCanvas");
-  var ctx = c.getContext("2d");
-</script>
-
-</body>
+  </body>
 </html>
--- main.js
+++ main.js
@@ -1,1 +1,94 @@
-//STOP
+
+
+var stored_fix = 0;
+var ratio = 0;
+var color = "red";
+
+// Runs the script on document loaded
+document.addEventListener("DOMContentLoaded", function(event) {
+  init();
+});
+
+function init() {
+  var c = document.getElementById("myCanvas");
+  var ctx = c.getContext("2d");
+  drawBoard();
+}
+
+
+function graph() {
+  clear();
+  drawBoard();
+  plotFixed();
+  colorLine();
+  plotAd();
+}
+
+function clear() {
+  ctx.beginPath();
+  ctx.clearRect(0,0, c.width, c.height);
+}
+
+function plotFixed() {
+  var fixed_cost = document.getElementById("fixed");
+  stored_fix = fixed_cost.value;
+  ratio = stored_fix / c.height;
+  ctx.beginPath();
+  //ctx.moveTo(0, (c.height - fixed_cost.value)); <!--origin-->
+  //ctx.lineTo(c.width, (c.height - fixed_cost.value));
+  ctx.moveTo(0, c.height - c.height/10);
+  ctx.lineTo(c.width, c.height - c.height/10);
+  ctx.strokeStyle = "blue";
+  if(fixed_cost.value > 0) {
+    ctx.stroke();
+  }
+}
+
+function plotAd() {
+  var ad_spend = document.getElementById("ad");
+  ctx.beginPath();
+  ctx.moveTo(0, (c.height - stored_fix));
+  ctx.lineTo(c.width, (c.height - stored_fix - ad_spend.value));
+  ctx.strokeStyle = color;
+  if(ad_spend.value > 0) {
+    ctx.stroke();
+  }
+}
+
+function reset() {
+  document.getElementById("ad").value = "";
+  document.getElementById("fixed").value = "";
+  document.getElementById("color").value = "#ff0000";
+  graph();
+}
+
+function about() {
+  window.alert("about");
+}
+
+function drawBoard() {
+// Box width
+var bw = c.width;
+// Box height
+var bh = c.height;
+// Padding
+var p = 0;
+
+ctx.beginPath();
+ctx.strokeStyle = "gainsboro";
+  for (var x = 0; x <= bw; x += 40) {
+      ctx.moveTo(0.5 + x + p, p);
+      ctx.lineTo(0.5 + x + p, bh + p);
+  }
+
+  for (var x = 0; x <= bh; x += 40) {
+      ctx.moveTo(p, 0.5 + x + p);
+      ctx.lineTo(bw + p, 0.5 + x + p);
+  }
+  ctx.stroke();
+}
+
+function colorLine() {
+  var revBox = document.getElementById("color");
+  color = revBox.value;
+}
